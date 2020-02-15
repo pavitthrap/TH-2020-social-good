d4da170b7bfccbe15c27212b21d3cf3b25b551a6
Pavitthra Pandurangan <pavitthp@andrew.cmu.edu>
2020-02-14 22:24:09 -0500

    update schema

--- app/flaskr/schema.sql
+++ app/flaskr/schema.sql
@@ -9,16 +9,29 @@ CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
+  user_type INTEGER NOT NULL,
+  query_list TEXT 
 );
 
-CREATE TABLE post (
+CREATE TABLE query (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
-  body TEXT NOT NULL,
+  subtitle TEXT NOT NULL,
+  pic_filename TEXT NOT NULL, 
+  category TEXT NOT NULL,
+  top_answer TEXT, 
+  answer_list TEXT, 
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+CREATE TABLE answer (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  upvotes INTEGER DEFAULT 0, 
+  downvotes INTEGER DEFAULT 0, 
+  query_id INTEGER NOT NULL
+)
 
 CREATE TABLE status (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
